703f39de23f9bb2b533ca087617d5363f08cab4c
Michelle <aragonmichelle.1106@gmail.com>
2023-03-30 14:40:13 -0400

    WorkingHoursSelector optimizated

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -21,14 +21,14 @@ export const Dropdown = ({ options, defaultOption, onSelectOption }) => {
   return (
       <div class="dropdown">
         <button
-          class="btn btn-sm mb-3 btn-light dropdown-toggle dropdown-footer d-flex w-100 "
+          class="btn btn-sm mb-3 bg-white text-dark dropdown-toggle dropdown-footer d-flex w-100 "
           type="button"
           data-bs-toggle="dropdown"
           aria-expanded="false"
         >
           {selectedOption.label}
         </button>
-        <ul class="dropdown-menu">{renderOptions}</ul>
+        <ul class="dropdown-menu dropdown-menu-options w-100">{renderOptions}</ul>
       </div>
   );
 };
